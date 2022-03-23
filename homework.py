0aa5a48f5cc94c7d728bb207801dd57e62be5253
Juniee5 <96186923+Juniee5@users.noreply.github.com>
2022-03-23 13:58:21 +0700

    boooott

--- homework.py
+++ homework.py
@@ -65,9 +65,8 @@ def timeout_and_logging(message: str = None, run_error=logging.error):
 
 
 def parse_status(homework: dict) -> str:
-    """
-    Извлекает из информации о конкретной домашней работе статус этой работы.
-    """
+    """Извлекает из информации о конкретной домашней 
+    работе статус этой работы."""
     logging.debug(f"Парсим домашнее задание: {homework}")
     homework_name = homework['homework_name']
     homework_status = homework['status']
